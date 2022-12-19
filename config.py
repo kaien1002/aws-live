010af6c75b45c2e6e0dd9b11fca5e27824462540
kaien1002 <120998355+kaien1002@users.noreply.github.com>
2022-12-19 11:41:47 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-customhost = "employee.cspnc5ouklrd.us-east-1.rds.amazonaws.com"
+customhost = "employee.cb8ckmeprbpu.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee"
+custombucket = "chokhaien-employee"
 customregion = "us-east-1"
